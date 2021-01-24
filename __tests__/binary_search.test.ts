eabb81742eef4aeab56ab2fc127287ca4a11d564
Dmitry Vdovichenko <kenar89@gmail.com>
2021-01-24 13:46:26 +0500

    add test for binary search

---
+++ __tests__/binary_search.test.ts
@@ -0,0 +1,10 @@
+import binary_search from "../src/binary_search";
+describe("Test binary search algorithm", () => {
+  test("search for item in sorted Array must return index or null", () => {
+    const list = [1, 3, 5, 7, 9],
+      item = 7,
+      nonExistItem = 2;
+    expect(binary_search(list, item)).toBe(3);
+    expect(binary_search(list, nonExistItem)).toBe(null);
+  });
+});
