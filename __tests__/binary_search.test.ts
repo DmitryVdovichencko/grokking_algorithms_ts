5c979a7d0724e77191fcc9cd1df577188c2dee06
Dmitry Vdovichenko <kenar89@gmail.com>
2021-02-07 12:42:30 +0500

    binary search finished

--- __tests__/binary_search.test.ts
+++ __tests__/binary_search.test.ts
@@ -3,8 +3,10 @@ describe("Test binary search algorithm", () => {
   test("search for item in sorted Array must return index or null", () => {
     const list = [1, 3, 5, 7, 9],
       item = 7,
+      firstItem = list[0],
       nonExistItem = 2;
     expect(binary_search(list, item)).toBe(3);
+    expect(binary_search(list, firstItem)).toBe(0);
     expect(binary_search(list, nonExistItem)).toBe(null);
   });
 });
--- src/binary_search.ts
+++ src/binary_search.ts
@@ -1,4 +1,49 @@
-const getSearchIndex = (length) => length/2;
-const binary_search = (list, item) => { 
-	list[getSearchIndex]
+const getMidIndex = (low, high) => Math.floor((low + high)/2);
+const binary_search = (list, item) => {
+	let low = 0, high = list.length-1;
+	while (low <= high) {
+		const mid = Math.floor((low + high)/2), guess = list[mid];
+		if( guess === item ){
+			return mid
+		}
+		else if(guess > item){
+			high = mid - 1
+		}
+		else{
+			low = mid +1
+		}
+	}
+	return null;
 }
+const binary_search_improved = (list, item, mid = getMidIndex(0,list.length-1)) => {
+	
+	if (list[mid] === item){
+		return mid
+	}
+
+	if (mid > 0){
+		const { low, high } = getLimits(list, mid, getSearchDir(list[mid],item));
+		return binary_search_improved(list, item, getMidIndex(low,high))
+	}
+
+	return null;
+}
+
+const getSearchDir = (guess, item) => guess > item ? "gt": "lt";
+
+const getLimits = (list, mid, searchDir) => {
+
+	const limitsByDirection={
+		gt:{
+			low: 0,
+			high: mid-1
+		},
+		lt:{
+			low: mid+1,
+			high: list.length-1
+		}
+	}
+	return limitsByDirection[searchDir]
+}
+
+export default binary_search_improved;
